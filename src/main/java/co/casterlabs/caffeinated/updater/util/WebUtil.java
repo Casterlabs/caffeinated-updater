b685dbbef4a74c7e2d88fb79a35af16638428cdd
e3ndr <33337309+e3ndr@users.noreply.github.com>
2024-12-03 12:37:52 -0600

    Unused code...

--- src/main/java/co/casterlabs/caffeinated/updater/util/WebUtil.java
+++ src/main/java/co/casterlabs/caffeinated/updater/util/WebUtil.java
@@ -1,8 +1,6 @@
 package co.casterlabs.caffeinated.updater.util;
 
 import java.io.IOException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -10,7 +8,6 @@ import java.net.http.HttpResponse.BodyHandler;
 import java.net.http.HttpResponse.BodyHandlers;
 
 import lombok.NonNull;
-import lombok.SneakyThrows;
 
 public class WebUtil {
     private static final HttpClient client = HttpClient.newHttpClient();
@@ -25,29 +22,6 @@ public class WebUtil {
 
     public static byte[] sendHttpRequestBytes(@NonNull HttpRequest.Builder builder) throws IOException, InterruptedException {
         return sendRawHttpRequest(builder, BodyHandlers.ofByteArray()).body();
-    }
-
-//    public static String escapeHtml(@NonNull String str) {
-//        return str
-//            .codePoints()
-//            .mapToObj(c -> c > 127 || "\"'<>&".indexOf(c) != -1 ? "&#" + c + ";" : new String(Character.toChars(c)))
-//            .collect(Collectors.joining());
-//    }
-
-    @SneakyThrows
-    public static String decodeURIComponent(@NonNull String s) {
-        return URLDecoder.decode(s, "UTF-8");
-    }
-
-    @SneakyThrows
-    public static String encodeURIComponent(@NonNull String s) {
-        return URLEncoder.encode(s, "UTF-8")
-            .replaceAll("\\+", "%20")
-            .replaceAll("\\%21", "!")
-            .replaceAll("\\%27", "'")
-            .replaceAll("\\%28", "(")
-            .replaceAll("\\%29", ")")
-            .replaceAll("\\%7E", "~");
     }
 
 }
